2c34f877a6dfe63c2b72d45a2c867e359cb58d96
Horia <thoria1999@gmail.com>
2016-05-25 11:41:40 +0300

    Category Model

---
+++ Category.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace KeepLearning
+{
+    public class Category
+    {
+        public Category()
+        {
+
+        }
+
+        public void GetSubcategoriesElements()
+        {
+            foreach(Category subcategory in Subcategories)
+            {
+                subcategory.GetSubcategoriesElements();
+
+                Words.AddRange(subcategory.Words);
+                ImagePaths.AddRange(ImagePaths);
+            }
+        }
+
+        public void DestroyUnusedSubcategories()
+        {
+            foreach (Category subcategory in Subcategories)
+            {
+                subcategory.Subcategories.Clear();
+            }
+        }
+
+        [XmlAttribute("name")]
+        public string Name;
+
+        [XmlArray("Subcategories")]
+        [XmlArrayItem("Category")]
+        IList<Category> Subcategories;
+
+        [XmlArray("Words")]
+        public List<string> Words;
+
+        [XmlArray("ImagePaths")]
+        public List<string> ImagePaths;
+    }
+}
