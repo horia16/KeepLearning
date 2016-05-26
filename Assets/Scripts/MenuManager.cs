e8e4a59db8a97b65079d99e54f17b212e18c33c4
Horia <thoria1999@gmail.com>
2016-05-26 09:33:24 +0300

    Asset for CategoryInfoMenu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+namespace KeepLearning
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public static CategoriesInfoContainer CategoriesInfoContainer;
+
+        public void Awake()
+        {
+            CategoriesInfoContainer = LoadCategoriesInfo();
+        }
+
+        public void ChangeMenu(GameObject oldMenu, GameObject newMenu)
+        {
+            oldMenu.SetActive(false);
+            newMenu.SetActive(true);
+        }
+
+        public static CategoriesInfoContainer LoadCategoriesInfo()
+        {
+            return LoadCategoriesInfo(Path.Combine(Environment.CurrentDirectory, "CategoriesInfo.xml"));
+        }
+
+        public static CategoriesInfoContainer LoadCategoriesInfo(string path)
+        {
+            var serializer = new XmlSerializer(typeof(CategoriesInfoContainer));
+            var stream = new FileStream(path, FileMode.Open);
+
+            CategoriesInfoContainer container = (CategoriesInfoContainer)serializer.Deserialize(stream);
+            stream.Close();
+
+            return container;
+        }
+    }
+}
