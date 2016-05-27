e87912ae3d4390716dc4751d6f39da2181cb6e77
Horia <thoria1999@gmail.com>
2016-05-27 19:10:30 +0300

    project

---
+++ KeepLearning/KeepLearning.CSharpTests/Assets/Scripts/ElementsFinder/ElementsFinderMenuTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ElementsFinderMenuTests
+    {
+        [TestMethod()]
+        public void CanChooseTest()
+        {
+            CategoriesInfoContainer container = CategoryInfo.LoadCategoriesInfo(Path.Combine(Environment.CurrentDirectory, "CategoryInfoTest.xml"));
+            container.GetSubcategoriesElements();
+            Assert.IsTrue(ElementsFinderMenu.CanChooseCategory(container.Categories[0].Subcategories[0]));
+        }
+    }
+}
